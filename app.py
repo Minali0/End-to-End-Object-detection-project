c61207bafbbe07ef7ae10231081a69a2c06d4c63
Minali0 <minali.jain@seaflux.tech>
2024-06-19 12:05:43 +0530

    logger and exception added

---
+++ app.py
@@ -0,0 +1,11 @@
+from signlanguage.logger import logging 
+from signlanguage.exception import SignException 
+import sys
+
+# logging.info("Welcome to the Project")
+
+try:
+    a = 7/'9'
+
+except Exception as e:
+    raise SignException(e,sys) from e 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signlanguage',
+    version= '0.0.0',
+    author= 'Minali0',
+    author_email= 'minali.jain@seaflux.tech',
+    packages= find_packages(),
+    install_requires = []
+
+)
